cfc01cf3a0eef68dac5d9449224e138337c8adf4
Danny <dhayek92@gmail.com>
2025-06-25 23:13:11 +0300

    Adjusted landing page structure further with new hidden fields for signup, began implementation of index.js script

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.10.0/+esm';
+
+const loginBtn = document.getElementById("login-btn");
+const signupBtn = document.getElementById("signup-btn");
