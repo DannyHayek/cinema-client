5f16f2dfec840d8354235154b30e2a46541d4ae2
Danny <dhayek92@gmail.com>
2025-06-27 11:09:39 +0300

    Implemented swapping between login and signup forms

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,39 @@
 import axios from 'https://cdn.jsdelivr.net/npm/axios@1.10.0/+esm';
 
 const loginBtn = document.getElementById("login-btn");
+const signupSwapBtn = document.getElementById("signup-swap-btn");
 const signupBtn = document.getElementById("signup-btn");
+const loginSwapBtn = document.getElementById("login-swap-btn");
+
+const loginDiv = document.getElementById("loginDiv");
+const signupDiv = document.getElementById("signupDiv");
+
+
+loginBtn.addEventListener("click", login);
+signupSwapBtn.addEventListener("click", signupSwap);
+signupBtn.addEventListener("click", signup);
+loginSwapBtn.addEventListener("click", loginSwap);
+
+function login() {
+    console.log("logging in...")
+
+
+}
+
+function signupSwap() {
+    console.log("switching...")
+
+    loginDiv.style.display = "none";
+    signupDiv.style.display = "flex";
+}
+
+function signup () {
+    console.log("signing up...")
+}
+
+function loginSwap () {
+    console.log("switching back...")
+
+    loginDiv.style.display = "flex";
+    signupDiv.style.display = "none";
+}
