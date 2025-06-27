f354bce4a9cc3c496cd7bb029f8f14c2701d478f
Danny <dhayek92@gmail.com>
2025-06-27 23:03:27 +0300

    Expanded basic framework of home page with a navbar, title and logo

--- scripts/home.js
+++ scripts/home.js
@@ -1,2 +1,6 @@
 const currentUser = localStorage.getItem("currentUser");
 console.log(currentUser);
+
+const welcomeMessage = document.getElementById("welcomeMessage");
+
+welcomeMessage.textContent = "Welcome, " + currentUser;
