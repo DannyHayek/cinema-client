bc313b5007611e07797497985f77e0a929eeae1f
Danny <dhayek92@gmail.com>
2025-06-28 19:53:39 +0300

    Implemented a function to allocate ids to every tile and to log that id in localStorage to allow for opening specific pages based on movie

--- scripts/home.js
+++ scripts/home.js
@@ -17,10 +17,25 @@ axios({
     console.log(response.data["movies"].length);
     for (let i = 0; i < response.data["movies"].length; i++){
        moviesGrid.innerHTML  += `
-            <article class="movie-tile flex column flex-space-between">
+            <article id="movie${response.data["movies"][i][0]}" class="movie-tile flex column flex-space-between">
                 <h3 class="light-font">${response.data["movies"][i][1]}</h3>
                 <h5 class="light-font tile-bottom-text">${response.data["movies"][i][2]}</h5>
             </article>
         ` 
     }
+}).then(function (e) {
+    const movieCards = document.querySelectorAll(".movie-tile");
+
+    for (let i = 0; i < movieCards.length; i++) {
+        let tile = movieCards[i];
+        tile.addEventListener("click", function () {
+            console.log(tile.id);
+            localStorage.setItem("currentMovie", tile.id)
+            // window.location.href = "../pages/quiz.html";
+        });
+
+        // console.log(localStorage.setItem("curretMovieID", );
+    }
+
+    // console.log(localStorage.getItem("currentQuiz"));
 });
