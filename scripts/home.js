904a7029ae985b90978d13f3b455428c13cfe9a8
Danny <dhayek92@gmail.com>
2025-06-28 19:06:10 +0300

    Implemented logic to fetch movie data from backend and dynamically create movie cards using that data

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,5 @@
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.10.0/+esm';
+
 const currentUser = localStorage.getItem("currentUser");
 console.log(currentUser);
 
@@ -7,10 +9,18 @@ welcomeMessage.textContent = "Welcome, " + currentUser;
 
 const moviesGrid = document.getElementById("moviesGrid");
 
-for (let i = 0; i < 10; i++) {
-    moviesGrid.innerHTML += `<article class="movie-tile flex column flex-space-between">
-                                <h3 class="light-font">Movie Name</h3>
-                                <h5 class="light-font tile-bottom-text">Synopsis</h5>
-                                <h5 class="light-font tile-bottom-text">Cast</h5>
-                            </article>`
-}
+axios({
+        method: "get",
+        url: "http://localhost/cinema-server/controllers/getMovies.php",
+    }
+).then(function (response) {
+    console.log(response.data["movies"].length);
+    for (let i = 0; i < response.data["movies"].length; i++){
+       moviesGrid.innerHTML  += `
+            <article class="movie-tile flex column flex-space-between">
+                <h3 class="light-font">${response.data["movies"][i][1]}</h3>
+                <h5 class="light-font tile-bottom-text">${response.data["movies"][i][2]}</h5>
+            </article>
+        ` 
+    }
+});
