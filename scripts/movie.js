f6bd9131d5797854305faece5949e298312ec2d4
Danny <dhayek92@gmail.com>
2025-06-29 09:11:24 +0300

    Created logic to allow fetching of movie data from backend and generating the movie details with this data, cast and genre pending

--- scripts/movie.js
+++ scripts/movie.js
@@ -6,3 +6,35 @@ console.log(currentUser);
 const welcomeMessage = document.getElementById("welcomeMessage");
 
 welcomeMessage.textContent = "Welcome, " + currentUser;
+
+const curMovieID = localStorage.getItem("currentMovie").at(5);
+
+console.log(curMovieID);
+
+const movieName = document.getElementById("movieName");
+const synopsis = document.getElementById("synopsis");
+const cast = document.getElementById("cast");
+const runtime = document.getElementById("runtime");
+const ageRating = document.getElementById("ageRating");
+const genre = document.getElementById("genre");
+const trailerLink = document.getElementById("trailerLink");
+
+
+
+axios({
+            method: "get",
+            url: "http://localhost/cinema-server/controllers/getMovies.php?",
+            params: {
+                id: curMovieID
+            }
+        }).then(function (response) {
+            const movie = response.data["movie"];
+            console.log(movie);
+
+            movieName.textContent = movie[1];
+            synopsis.textContent = movie[2];
+            runtime.textContent = movie[3] + " minutes";
+            ageRating.textContent = movie[4];
+            trailerLink.textContent = movie[5];
+        }
+);
