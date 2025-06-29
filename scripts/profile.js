aaf94b9fd5fd45c135dabca38c63e4c1451456f0
Danny <dhayek92@gmail.com>
2025-06-29 16:05:36 +0300

    Set up framework to allow for changing profile details

--- scripts/profile.js
+++ scripts/profile.js
@@ -16,8 +16,26 @@ const userAge = document.getElementById("userAge");
 const userPassword = document.getElementById("userPassword");
 const userGenre = document.getElementById("userGenre");
 
+const chngName = document.getElementById("changeName");
+const chngEmail = document.getElementById("changeEmail");
+const chngNumber = document.getElementById("changeNumber");
+const chngAge = document.getElementById("changeAge");
+const chngPass = document.getElementById("changePass");
+const chngGenre = document.getElementById("changeGenre");
 
-axios({
+const newName = document.getElementById("newName");
+const newEmail = document.getElementById("newEmail");
+const newNumber = document.getElementById("newNumber");
+const newAge = document.getElementById("newAge");
+const newPass = document.getElementById("newPass");
+// const newGenre = document.getElementById("newName");
+
+
+fetchInfo();
+
+
+function fetchInfo () {
+    axios({
             method: "get",
             url: "http://localhost/cinema-server/controllers/getUsers.php?",
             params: {
@@ -25,6 +43,7 @@ axios({
             }
         }).then(function (response) {
             console.log(response.data);
+            let userID =  response.data[0];
             userName.textContent = response.data[1];
             userEmail.textContent = response.data[2];
             userNumber.textContent = response.data[3];
@@ -35,5 +54,9 @@ axios({
             }
 
             userGenre.textContent = response.data[6];
+        }).then(function (userID) {
+
         });
+}
+
 
