8688ff70175347e9efb776b4b98dba82effd3138
Danny <dhayek92@gmail.com>
2025-06-29 12:13:55 +0300

    Implemented logic to receive genres from backend and dynamically display them

--- scripts/movie.js
+++ scripts/movie.js
@@ -29,12 +29,23 @@ axios({
             }
         }).then(function (response) {
             const movie = response.data["movie"];
+            const genreData = response.data["genre"];
             console.log(movie);
+            console.log(genreData);
+
 
             movieName.textContent = movie[1];
             synopsis.textContent = movie[2];
             runtime.textContent = movie[3] + " minutes";
             ageRating.textContent = movie[4];
             trailerLink.textContent = movie[5];
+
+            for (let i = 0; i < genreData.length; i++) {
+                console.log(genreData[i][1]);
+                genre.textContent += genreData[i][1];
+                if (i + 1 != genreData.length) {
+                    genre.textContent += ", ";
+                }
+            }
         }
 );
