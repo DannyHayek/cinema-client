12a628f89d9d956f03dba2a202e850f1362c40ae
Danny <dhayek92@gmail.com>
2025-06-29 15:16:24 +0300

    Implemented logic to dynamically generate profile details after api call

--- scripts/profile.js
+++ scripts/profile.js
@@ -9,11 +9,31 @@ const welcomeMessage = document.getElementById("welcomeMessage");
 
 welcomeMessage.textContent = "Welcome, " + currentUser;
 
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
+const userNumber = document.getElementById("userNumber");
+const userAge = document.getElementById("userAge");
+const userPassword = document.getElementById("userPassword");
+const userGenre = document.getElementById("userGenre");
+
+
 axios({
             method: "get",
             url: "http://localhost/cinema-server/controllers/getUsers.php?",
             params: {
                 email: currentEmail
             }
-        }).then(response => console.log(response.data));
+        }).then(function (response) {
+            console.log(response.data[1]);
+            userName.textContent = response.data[1];
+            userEmail.textContent = response.data[2];
+            userNumber.textContent = response.data[3];
+            userAge.textContent = response.data[5];
+
+            for (let i = 0; i < response.data[4].length; i++) {
+                userPassword.textContent += "*";
+            }
+
+            userGenre.textContent = response.data[6];
+        });
 
